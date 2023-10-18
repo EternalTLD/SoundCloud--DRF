7832a57f14a34cdc32c5f744d2409add928d8386
EternalTLD <shvejkin01@mail.ru>
2023-10-18 23:09:00 +0300

    add permissions

---
+++ src/base/services.py
@@ -0,0 +1,40 @@
+from django.core.exceptions import ValidationError
+from django.conf import settings
+
+
+def get_upload_profile_image_path(instance, file):
+    """
+    Returns file path to profile image.
+    Format: media/profile_images/user_id/photo.jpg
+    """
+    return f"profile_images/user_{instance.id}/{file}"
+
+
+def get_upload_album_cover_path(instance, file):
+    """
+    Returns file path to albums cover.
+    Format: media/album_covers/user_id/photo.jpg
+    """
+    return f"album_covers/user_{instance.id}/{file}"
+
+
+def get_upload_playlist_cover_path(instance, file):
+    """
+    Returns file path to playlist cover.
+    Format: media/playlist_covers/user_id/photo.jpg
+    """
+    return f"playlist_covers/user_{instance.id}/{file}"
+
+
+def get_upload_audio_path(instance, file):
+    """
+    Returns file path to audio file.
+    Format: media/audios/user_id/audio.mp3
+    """
+    return f"audios/user_{instance.id}/{file}"
+
+
+def validate_image_size(file_object):
+    """File size validator"""
+    if file_object.size > settings.PROFILE_IMAGE_SIZE_MB_LIMIT * 1024 * 1024:
+        raise ValidationError(f"Max size of image is {settings.PROFILE_IMAGE_SIZE_MB_LIMIT} MB")
