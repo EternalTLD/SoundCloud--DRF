d6f3b8f0ed193c369ca35da2554c50c81f159c5c
EternalTLD <shvejkin01@mail.ru>
2023-10-19 22:19:17 +0300

    fix pathes

--- src/base/services.py
+++ src/base/services.py
@@ -7,23 +7,15 @@ def get_upload_profile_image_path(instance, file):
     Returns file path to profile image.
     Format: media/profile_images/user_id/photo.jpg
     """
-    return f"profile_images/user_{instance.id}/{file}"
+    return f"profile_images/user_{instance.user.id}/{file}"
 
 
 def get_upload_album_cover_path(instance, file):
     """
     Returns file path to albums cover.
-    Format: media/album_covers/user_id/photo.jpg
+    Format: media/album_covers/album_id/photo.jpg
     """
-    return f"album_covers/user_{instance.id}/{file}"
-
-
-def get_upload_playlist_cover_path(instance, file):
-    """
-    Returns file path to playlist cover.
-    Format: media/playlist_covers/user_id/photo.jpg
-    """
-    return f"playlist_covers/user_{instance.id}/{file}"
+    return f"albums/album_{instance.id}/cover/{file}"
 
 
 def get_upload_audio_path(instance, file):
@@ -31,7 +23,15 @@ def get_upload_audio_path(instance, file):
     Returns file path to audio file.
     Format: media/audios/user_id/audio.mp3
     """
-    return f"audios/user_{instance.id}/{file}"
+    return f"albums/album_{instance.album.id}/audios/{file}"
+
+
+def get_upload_playlist_cover_path(instance, file):
+    """
+    Returns file path to playlist cover.
+    Format: media/playlist_covers/user_id/photo.jpg
+    """
+    return f"playlist_covers/user_{instance.user.id}/{file}"
 
 
 def validate_image_size(file_object):
