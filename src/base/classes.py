576d4534a338ede62dd535508c68fc58c1802d11
EternalTLD <shvejkin01@mail.ru>
2023-10-28 19:34:47 +0300

    add mixed serializer

---
+++ src/base/classes.py
@@ -0,0 +1,9 @@
+class MixedSerializer:
+    """Serializer actions mixin"""
+    def get_serializer(self, *args, **kwargs):
+        try:
+            serializer_class = self.serializer_classes_by_action[self.action]
+        except KeyError:
+            serializer_class = self.get_serializer_class()
+        kwargs.setdefault('context', self.get_serializer_context())
+        return serializer_class(*args, **kwargs)
