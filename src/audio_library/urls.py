dd1b90b60b5281c46d2c66a68db7e2fb8da496c5
EternalTLD <shvejkin01@mail.ru>
2024-02-08 01:03:39 +0300

    Added routers

--- src/audio_library/urls.py
+++ src/audio_library/urls.py
@@ -1,37 +1,19 @@
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework.routers import SimpleRouter
 
 from . import views
 
 
 app_name = "audio"
 
+router = SimpleRouter()
+router.register(r"album", views.AlbumViewSet)
+router.register(r"audio", views.AudioViewSet)
+router.register(r"playlist", views.PlaylistViewSet)
+router.register(r"comment", views.CommentViewSet)
+
 urlpatterns = [
-    path("genre/", views.GenreListView.as_view()),
-    path("license/", views.LicenseView.as_view({"get": "list", "post": "create"})),
-    path(
-        "license/<int:pk>/",
-        views.LicenseView.as_view(
-            {"put": "update", "delete": "destroy", "get": "retrieve"}
-        ),
-    ),
-    path("album/", views.AlbumView.as_view({"get": "list", "post": "create"})),
-    path(
-        "album/<int:pk>/",
-        views.AlbumView.as_view({"put": "update", "delete": "destroy"}),
-    ),
-    path("author_album/<int:pk>/", views.PublicAlbumListView.as_view()),
-    path("audio/", views.AudioView.as_view({"get": "list", "post": "create"})),
-    path(
-        "audio/<int:pk>/",
-        views.AudioView.as_view({"put": "update", "delete": "destroy"}),
-    ),
-    path("stream_audio/<int:pk>/", views.StreamAudioView.as_view()),
-    path("download_audio/<int:pk>/", views.DownloadAudioView.as_view()),
-    path("audio_list/", views.AllAudioView.as_view()),
-    path("author_audio_list/<int:pk>/", views.AuthorAudioView.as_view()),
-    path("playlist/", views.PlaylistView.as_view({"get": "list", "post": "create"})),
-    path(
-        "playlist/<int:pk>/",
-        views.PlaylistView.as_view({"put": "update", "delete": "destroy"}),
-    ),
+    path("genre/", views.GenreListAPIView.as_view()),
+    path("", include(router.urls)),
 ]
