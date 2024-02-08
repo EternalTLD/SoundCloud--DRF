cccc685093baad1e716fb1d78f324f209a89ec99
EternalTLD <shvejkin01@mail.ru>
2024-02-08 19:10:11 +0300

    Added user and author permissions

--- src/base/permissions.py
+++ src/base/permissions.py
@@ -1,6 +1,30 @@
+from django.contrib.auth import get_user_model
 from rest_framework import permissions
 
 
-class IsAuthor(permissions.IsAuthenticated):
+User = get_user_model()
+
+
+class IsAuthor(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.user == request.user
+
+
+class IsAuthorOrReadOnly(IsAuthor):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return super().has_object_permission(request, view, obj)
+
+
+class IsUser(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if isinstance(obj, User):
+            return obj == request.user
+
+
+class IsUserOrReadOnly(IsUser):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return super().has_object_permission(request, view, obj)
